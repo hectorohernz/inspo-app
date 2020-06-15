f55463f04cb9e996c4e80b6a4f6ea8425c499e1c
Hector Hernandez <hectorohernz@gmail.com>
2020-06-15 07:48:37 -0400

    added layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import content from './content.json';
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      photos:content
+    }
+  }
+ 
+  render() {
+    return (
+      <section className="main">
+        <nav className="nav">
+          <h1>KIN</h1>
+          <p>Created By Hector</p>
+        </nav>
+        <div className="draggable-photos">
+          {this.state.photos.content.map( item => 
+            <img src={item.link}/>
+          )}
+        </div>
+      </section>
+    )
+  }
 }
 
 export default App;
